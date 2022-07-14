e8dc023f1be52522b3fd892dace083f4526e77a7
Iuri Freire <iuricostafreire@gmail.com>
2022-07-14 08:46:40 -0300

    Lesson 22

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,43 +1,3 @@
-// const user_goal_app = Vue.createApp({
-//     data() {
-//         return {
-//             goals: [],
-//             enteredValue: "",
-//         };
-//     },
-//     methods: {
-//         addGoal() {
-//             this.goals.push(this.enteredValue);
-//             this.enteredValue = "";
-//         },
-//     },
-// }).mount("#app");
-
-
-// const app_user_goal = Vue.createApp({
-//     data() {
-//         return {
-//             courseGoal: "Learn VueJS!",
-//             courseGoalA: "Finish the curse and learn VueJS!",
-//             courseGoalB: "<h2>Master VueJS and build amazing apps!</h2>",
-//             vueLink: "https://vuejs.org"
-//         };
-//     },
-//     methods: {
-//         outputGoal() {
-//             const randomNumber = Math.random();
-
-//             if (randomNumber < 0.5)
-//                 return this.courseGoalA;
-//             else
-//                 return this.courseGoalB;
-//         }
-//     }
-// });
-
-// app_user_goal.mount('#user-goal');
-
-
 const app = Vue.createApp({
     data() {
         return {
@@ -45,10 +5,10 @@ const app = Vue.createApp({
         };
     },
     methods: {
-        add() {
+        addCounter() {
             this.counter++;
         },
-        sub() {
+        subCounter() {
             this.counter--;
         }
     }
