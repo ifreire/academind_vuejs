06e9a4cc4ef967ae245f5fd6d5f0707e27da759a
Iuri Freire <iuricostafreire@gmail.com>
2022-09-20 07:15:46 -0300

    Lesson 47

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,134 +1,21 @@
-const events = Vue.createApp({
+const app = Vue.createApp({
     data() {
         return {
-            counter: 0,
-            name: "",
-            lastName: "",
-            confirmedName: "",
+            goals: [],
+            enteredGoalValue: ''
         };
     },
     methods: {
-        addCounter(num) {
-            this.counter += num;
-        },
-        subCounter(num) {
-            this.counter -= num;
-        },
-        setName(event) {
-            this.name = event.target.value;
-        },
-        confirmName() {
-            this.confirmedName = this.name;
-        },
-        resetInput() {
-            this.name = "";
-        },
-        outputFullname() {
-            console.log('outputFullname');
+        addGoal() {
+            if (this.enteredGoalValue.trim() !== "")
+                this.goals.push(this.enteredGoalValue.trim());
 
-            if (this.name === '')
-                return '';
-            
-            return this.name + ' ' + 'Freire';
-        }
-    },
-    computed: {
-        fullname() {
-            console.log('fullname');
-            
-            if (this.name === '' || this.lastName === '')
-                return '';
-            
-            return this.name + ' ' + this.lastName;
-        }
-    },
-    watch: {
-        counter(value) {
-            if (value > 50) {
-                const that = this;
-                setTimeout(function() {
-                    that.counter = 0;
-                }, 2000);
-            }
+            this.enteredGoalValue = "";
+        },
+        removeGoal(idx) {
+            this.goals.splice(idx, 1);
         }
     }
 });
 
-const styling_click = Vue.createApp({
-    data() {
-        return {
-            boxAselected: false,
-            boxBselected: false,
-            boxCselected: false,
-        };
-    },
-    methods: {
-        boxSelected(box) {
-            if (box === "A")
-                this.boxAselected = !this.boxAselected;
-            else if (box === "B")
-                this.boxBselected = !this.boxBselected;
-            else if (box === "C")
-                this.boxCselected = !this.boxCselected;
-        }
-    },
-    computed: {
-        boxAClasses() {
-            return ['demo', { activeA: this.boxAselected }];
-        },
-        boxBClasses() {
-            return { activeB: this.boxBselected };
-        },
-        boxCClasses() {
-            return { activeC: this.boxCselected };
-        }
-    }
-});
-
-const styling_mouseover = Vue.createApp({
-    data() {
-        return {
-            boxAselected: false,
-            boxBselected: false,
-            boxCselected: false,
-            boxDSelected: false,
-        };
-    },
-    methods: {
-        boxOver(box) {
-            if (box === "A")
-                this.boxAselected = !this.boxAselected;
-            else if (box === "B")
-                this.boxBselected = !this.boxBselected;
-            else if (box === "C")
-                this.boxCselected = !this.boxCselected;
-            else if (box === "D")
-                this.boxDSelected = !this.boxDSelected;
-        },
-        boxOut(box) {
-            if (box === "A")
-                this.boxAselected = !this.boxAselected;
-            else if (box === "B")
-                this.boxBselected = !this.boxBselected;
-            else if (box === "C")
-                this.boxCselected = !this.boxCselected;
-            else if (box === "D")
-                this.boxDSelected = !this.boxDSelected;
-        }
-    },
-    computed: {
-        boxAClasses() {
-            return { activeA: this.boxAselected };
-        },
-        boxBClasses() {
-            return { activeB: this.boxBselected };
-        },
-        boxCClasses() {
-            return { activeC: this.boxCselected };
-        }
-    }
-});
-
-events.mount('#events');
-styling_click.mount('#styling_click');
-styling_mouseover.mount("#styling_mouseover");
+app.mount('#user-goals');
