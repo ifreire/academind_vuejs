51d293b0d16c5bd6d6ae0e3f8ce95b866fb0e8e9
Iuri Freire <iuricostafreire@gmail.com>
2023-05-02 00:36:17 -0300

    Section 5 - Behind the scenes.

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,9 +10,34 @@ const app = Vue.createApp({
             this.currentUserInput = event.target.value;
         },
         setText() {
-            this.message = this.currentUserInput;
+            // this.message = this.currentUserInput;
+            this.message = this.$refs.userText.value;
+        },
+    },
+});
+
+const app2 = Vue.createApp({
+    // template: `
+    //     <h2>Favorite Meal</h2>
+    //     <input type="text" @input="saveInput" />
+    //     <button @click="setText">Set Text</button>    
+    //     <p>{{ message }}</p>
+    // `,
+    data() {
+        return {
+            favoriteMeal: "",
+            message: "I love PIZZA!",
+        };
+    },
+    methods: {
+        saveInput(event) {
+            this.favoriteMeal = event.target.value;
+        },
+        setText() {
+            this.message = this.favoriteMeal;
         },
     },
 });
 
 app.mount("#app");
+app2.mount("#app2");
