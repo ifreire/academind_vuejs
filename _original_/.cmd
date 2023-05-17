4bb18aa39ac90f02e6a41f4b27afc1cfd6b72637
Iuri Freire <iuricostafreire@gmail.com>
2023-05-17 00:23:59 -0300

    ...

--- _original_/.cmd
+++ _original_/.cmd
@@ -1,2 +1,4 @@
-deno run -A npm:create-vite-extra .
+git remote set-url origin https://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@github.com/ifreire/academind_vuejs.git
 
+
+deno run -A npm:create-vite-extra .
